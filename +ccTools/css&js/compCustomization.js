efcb0a3ccf4f751d8c73c71d0e4babd9240ddc52
Eric <75496918+EricMagalhaesDelgado@users.noreply.github.com>
2024-05-20 23:22:04 -0400

    ProgressDialogV2
    webapp compatible

--- +ccTools/css&js/compCustomization.js
+++ +ccTools/css&js/compCustomization.js
@@ -7,20 +7,30 @@ function setup(htmlComponent) {
         }
     });
 
-    htmlComponent.addEventListener("addKeyDownListener", function(event) {
-        let objDataName  = event.Data.DataName.toString();
-        let objDataTag   = event.Data.DataTag.toString();
-        let objHandle    = window.parent.document.querySelector(`div[data-tag="${objDataTag}"]`).children[0];
-
-        objHandle.addEventListener('keydown', function(event) {
-            keyDownListener(event, htmlComponent, objDataName, objHandle);
+    htmlComponent.addEventListener("onBeforeTabClose", function() {
+        window.parent.parent.addEventListener("beforeunload", function(event) {
+            htmlComponent.sendEventToMATLAB("onBeforeTabClose");
         });
     });
 
-    htmlComponent.addEventListener("EditFieldFocus", function(event) {
-        let objDataName  = event.Data.DataName.toString();
-        let objDataTag   = event.Data.DataTag.toString();
-        let objHandle    = window.parent.document.querySelector(`div[data-tag="${objDataTag}"]`).querySelector('input');
+    htmlComponent.addEventListener("addKeyDownListener", function(event) {
+        let objDataName  = event.Data.componentName.toString();
+        let objDataTag   = event.Data.componentDataTag.toString();
+        let objHandle    = window.parent.document.querySelector(`div[data-tag="${objDataTag}"]`).children[0];
+
+        objHandle.addEventListener("keydown", function(event) {
+            let keyEvents = ["ArrowUp", "ArrowDown", "Enter", "Escape"];
+        
+            if (keyEvents.includes(event.key)) {
+                htmlComponent.sendEventToMATLAB(objDataName, event.key);
+            }
+        });
+    });
+
+    htmlComponent.addEventListener("setFocus", function(event) {
+        let objDataName  = event.Data.componentName.toString();
+        let objDataTag   = event.Data.componentDataTag.toString();
+        let objHandle    = window.parent.document.querySelector(`div[data-tag="${objDataTag}"]`).querySelector("input");
 
         try {
             objHandle.focus();
@@ -35,10 +45,134 @@ function setup(htmlComponent) {
             var className       = event.Data.className.toString();
             var classAttributes = event.Data.classAttributes.toString();
     
-            var s = document.createElement('style');
-            s.type = 'text/css';
-            s.appendChild(document.createTextNode(className + " { " + classAttributes + " }"));    
+            var s = document.createElement("style");
+            s.type = "text/css";
+            s.appendChild(document.createTextNode(className + " { " + classAttributes + " }"));
             window.parent.document.head.appendChild(s);
+        } catch (ME) {
+            // console.log(ME)
+        }
+    });
+
+    htmlComponent.addEventListener("progressDialog", function(event) {
+        try {
+            var Type = event.Data.Type.toString();
+            var UUID = event.Data.UUID.toString();
+    
+            switch (Type) {
+                case "changeVisibility":
+                    var Visible  = event.Data.Visibility.toString();
+                    var elements = window.parent.document.querySelectorAll(`div[data-tag="${UUID}"]`);                    
+                    elements.forEach(element => {
+                        element.style.visibility = Visible;
+                    });
+                    break;
+
+                case "changeColor":
+                    var newColor = event.Data.Color.toString();
+                    window.parent.document.documentElement.style.setProperty("--sk-color", newColor);
+                    break;
+
+                case "changeSize":
+                    var newSize  = event.Data.Size.toString();
+                    window.parent.document.documentElement.style.setProperty("--sk-size", newSize);
+                    break;
+
+                case "Creation":
+                    var zIndex = 1000;
+                    var Size   = event.Data.Size.toString();
+                    var Color  = event.Data.Color.toString();
+
+                    // Style
+                    var s = document.createElement("style");
+                    s.type = "text/css";                    
+                    s.innerHTML = `
+                        :root {
+                            --sk-size: ${Size};
+                            --sk-color: ${Color};
+                        }
+                        
+                        .sk-chase {
+                            width: var(--sk-size);
+                            height: var(--sk-size);
+                            position: relative;
+                            animation: sk-chase 2.5s infinite linear both; 
+                        }
+                        
+                        .sk-chase-dot {
+                            width: 100%;
+                            height: 100%;
+                            position: absolute;
+                            left: 0;
+                            top: 0; 
+                            animation: sk-chase-dot 2.0s infinite ease-in-out both; 
+                        }
+                        
+                        .sk-chase-dot:before {
+                            content: "";
+                            display: block;
+                            width: 25%;
+                            height: 25%;
+                            background-color: var(--sk-color);
+                            border-radius: 100%;
+                            animation: sk-chase-dot-before 2.0s infinite ease-in-out both; 
+                        }
+                        
+                        .sk-chase-dot:nth-child(1) { animation-delay: -1.1s; }
+                        .sk-chase-dot:nth-child(2) { animation-delay: -1.0s; }
+                        .sk-chase-dot:nth-child(3) { animation-delay: -0.9s; }
+                        .sk-chase-dot:nth-child(4) { animation-delay: -0.8s; }
+                        .sk-chase-dot:nth-child(5) { animation-delay: -0.7s; }
+                        .sk-chase-dot:nth-child(6) { animation-delay: -0.6s; }
+                        .sk-chase-dot:nth-child(1):before { animation-delay: -1.1s; }
+                        .sk-chase-dot:nth-child(2):before { animation-delay: -1.0s; }
+                        .sk-chase-dot:nth-child(3):before { animation-delay: -0.9s; }
+                        .sk-chase-dot:nth-child(4):before { animation-delay: -0.8s; }
+                        .sk-chase-dot:nth-child(5):before { animation-delay: -0.7s; }
+                        .sk-chase-dot:nth-child(6):before { animation-delay: -0.6s; }
+                        
+                        @keyframes sk-chase {
+                            100% { transform: rotate(360deg); } 
+                        }
+                        
+                        @keyframes sk-chase-dot {
+                            80%, 100% { transform: rotate(360deg); } 
+                        }
+                        
+                        @keyframes sk-chase-dot-before {
+                            50% {
+                                transform: scale(0.4); 
+                            } 100%, 0% {
+                                transform: scale(1.0); 
+                            } 
+                        }
+                    `;
+
+                    // Background layer
+                    var u = document.createElement("div");
+                    u.setAttribute("data-tag", UUID);
+                    u.style.cssText = "visibility: hidden; position: absolute; left: 0%; top: 0%; width: 100%; height: 100%; background-color: rgba(255, 255, 255, 0.65); z-index: " + (zIndex + 1) + ";";
+
+                    // Progress dialog
+                    var w = document.createElement("div");
+                    w.setAttribute("data-tag", UUID);
+                    w.style.cssText = "visibility: hidden; position: absolute; left: 50%; top: 50%; transform: translate(-50%, -50%); z-index: " + (zIndex + 2) + ";";
+                    w.innerHTML     = `
+                        <div class="sk-chase">
+                            <div class="sk-chase-dot"></div>
+                            <div class="sk-chase-dot"></div>
+                            <div class="sk-chase-dot"></div>
+                            <div class="sk-chase-dot"></div>
+                            <div class="sk-chase-dot"></div>
+                            <div class="sk-chase-dot"></div>
+                        </div>
+                    `;
+                    
+                    window.parent.document.head.appendChild(s);
+                    window.parent.document.body.appendChild(u);
+                    window.parent.document.body.appendChild(w);
+                    break;
+            };
         } catch (ME) {
             // console.log(ME)
         }
@@ -109,10 +243,6 @@ function setup(htmlComponent) {
                                 elements[ii].style[objProperty] = objValue;
                             }
                     }
-                    
-                case "matlab.ui.container.Tab":
-                    // Pendente!
-                    return;
                 
                 case "matlab.ui.control.Button":
                 case "matlab.ui.control.DropDown":
@@ -173,30 +303,11 @@ function setup(htmlComponent) {
                             for (let ii = 0; ii < elements.length; ii++) {
                                 elements[ii].style[objProperty] = objValue;
                             }
+                            break;
                     }
             }
         } catch (ME) {
             // console.log(ME)
         }
     });
-}
-
-function keyDownListener(event, htmlComponent, objDataName, objHandle) {
-    keyEvents = ["ArrowUp", "ArrowDown", "Enter", "Escape"];
-
-    switch (objDataName) {
-        case "app.EditField":
-            break;
-        case "app.ListBox":
-            let selectedItem = objHandle.getElementsByClassName("mwListItem mwListItemSelected");
-
-            if (selectedItem.length == 1) {
-                selectedItem[0].dataset.mwIndex != "1"
-            }
-            break;
-    }
-
-    if (keyEvents.includes(event.key)) {
-        htmlComponent.sendEventToMATLAB(event.key, objDataName);
-    }
 }
